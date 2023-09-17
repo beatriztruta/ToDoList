a4b1d8653879a6def841677500a03ea7b9b781c3
Ana Beatriz Truta <ana.truta@ccc.ufcg.edu.br>
2023-09-17 01:00:34 -0300

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,11 +1,7 @@
 module.exports = {
-    // Chave secreta para autenticação JWT (exemplo)
-    jwtSecret: 'sua_chave_secreta_aqui',
+       jwtSecret: 'sua_chave_secreta_aqui',
   
-    // Configurações do banco de dados MongoDB (exemplo)
-    mongoURI: 'mongodb://localhost:27017/minha-base-de-dados',
+     mongoURI: 'mongodb://localhost:27017/minha-base-de-dados',
   
-    // Outras configurações sensíveis (exemplo)
-    // apiKey: 'sua_api_key_aqui',
-  };
+    };
   
